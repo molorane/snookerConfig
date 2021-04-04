60dbd7369e463e4a96c2f2c08e4d830cc4fd1424
Molorane Mothusi <molorane.mothusi@gmail.com>
2021-04-04 15:03:56 +0200

    first commit

---
+++ src/test/java/org/dclm/sealConfig/SealConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package org.dclm.sealConfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SealConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
